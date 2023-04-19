d14e1519658c6a26560a1b769a24ffe6e5d5610a
BLACKBOX\paul <paul@grebenc.ca>
2023-04-19 15:11:50 -0400

    Remove duplicate test.

--- RedisTests/TransactionTests.cs
+++ RedisTests/TransactionTests.cs
@@ -9,24 +9,6 @@ namespace RedisTests;
 public class TransactionTests : RedisTestFixture
 {
     [Test]
-    public async Task CanExecuteMultipleCommandsInATransactionOriginal()
-    {
-        var transaction = Database.CreateTransaction();
-
-        var task1 = transaction.StringSetAsync("foo1", "bar1");
-        var task2 = transaction.StringSetAsync("foo2", "bar2");
-
-        var committed = await transaction.ExecuteAsync();
-        committed.Should().BeTrue();
-        task1.IsCompletedSuccessfully.Should().BeTrue();
-        task2.IsCompletedSuccessfully.Should().BeTrue();
-
-        (await Database.StringGetAsync("foo1")).Should().Be("bar1");
-        (await Database.StringGetAsync("foo2")).Should().Be("bar2");
-    }
-
-
-    [Test]
     public async Task CanExecuteMultipleCommandsInATransaction()
     {
         var transaction = Database.CreateTransaction();
